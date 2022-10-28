5ef36ebb8c8c649b32cbcdc8a94b028aaec4e447
22-hop2-udval <22-hop2-udval@lab-1-14s-iMac.local>
2022-10-28 17:13:09 +0800

    u

--- React JS/Day5/user/src/component/FindOne.jsx
+++ React JS/Day5/user/src/component/FindOne.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-
+import users from "../users.json";
 const FindOne = () => {
   const { username } = useParams();
+  const user = users.find((user) => user.username === username);
   return (
     <div>
       <div>user one : {username}</div>
+      <div>addresss : {user.username}</div>
     </div>
   );
 };
---
+++ React JS/Day9/stopwatch/src/components/stopwatch.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Stopwatch = () => {
+  return (
+    <div>
+      <div>00:00:00</div>
+      <div>
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </div>
+    </div>
+  );
+};
