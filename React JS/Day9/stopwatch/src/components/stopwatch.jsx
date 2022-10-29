e00e95dd2ac305df593b626d10918f11467a07ae
Uda <uda@Udas-MacBook-Pro.local>
2022-10-29 20:48:10 +0800

    stopwatch duussaa

--- React JS/Day9/stopwatch/src/components/stopwatch.jsx
+++ React JS/Day9/stopwatch/src/components/stopwatch.jsx
@@ -6,19 +6,42 @@ import { Timer } from "./timer";
 export const Stopwatch = () => {
   const [time, setTime] = useState(0);
   const [start, setStart] = useState(false);
+  useEffect(() => {
+    let interval = null;
+    if (start === true) {
+      interval = setInterval(() => {
+        setTime((time) => time + 10);
+      }, 10);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [start]);
+
   const handleStart = () => {
-    setStart(true);
+    setStart(!start);
+  };
+  const handleReset = () => {
+    setStart(false);
+    setTime(0);
   };
 
   return (
-    <div>
+    <div className="stopwatchContainer">
       <div>
         <Timer time={time} />
       </div>
-      <div>
-        <button onClick={handleStart}>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+      <div className="buttonContainer">
+        <button className="Button1" onClick={handleStart}>
+          {start === false && time === 0 && "Start"}
+          {start === false && time !== 0 && "Resume"}
+          {start === true && "Stop"}
+        </button>
+        {time !== 0 && (
+          <button className="resetButton" onClick={handleReset}>
+            Reset
+          </button>
+        )}
       </div>
     </div>
   );
--- React JS/Day9/stopwatch/src/components/timer.jsx
+++ React JS/Day9/stopwatch/src/components/timer.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 export const Timer = (props) => {
   return (
-    <div>
-      <div></div>
+    <div className="timerContainer">
+      <div className="tsag">
+        {("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}
+      </div>
+      <div className="temdeg">:</div>
+      <div className="tsag">
+        {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}
+      </div>
+      <div className="temdeg">.</div>
+      <div className="tsag">{("0" + ((props.time / 10) % 100)).slice(-2)}</div>
     </div>
   );
 };
