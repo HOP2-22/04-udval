ac5240a1a77eb15053f07c2585235e1ea9b7e014
22-hop2-udval <22-hop2-udval@lab-1-14s-iMac.local>
2022-12-07 19:22:30 +0800

    flipcard

--- DC/flipGame/flip/src/App.js
+++ DC/flipGame/flip/src/App.js
@@ -1,10 +1,77 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Card } from "./components/Card";
 
 function App() {
+  const shapes = ["Heart", "Cloud", "Moon", "Snowflake", "Sun", "SomeGuy"];
+  const [cards, setCards] = useState([]);
+  const generate = () => {
+    let newCards = [...shapes, ...shapes].sort(() => 0.5 - Math.random());
+    setCards(newCards);
+  };
+  useEffect(() => {
+    generate();
+  }, []);
+  const [flipped, setFlipped] = useState(new Array(12).fill(false));
+  const [first, setFirst] = useState(null);
+  const flip = (index) => {
+    setFlipped(flipped.map((f, i) => (i === index ? !f : f)));
+    if (first === null) {
+      setFirst(index);
+      return;
+    }
+    if (cards[first] === cards[index]) {
+      setTimeout(() => {
+        setFirst(null);
+        setCards(cards.map((card, i) => (card === cards[first] ? null : card)));
+      }, 600);
+    } else {
+      setTimeout(() => {
+        setFlipped(new Array(12).fill(false));
+        setFirst(null);
+      }, 600);
+    }
+  };
+  // const [isEnd, setIsEnd] = useState(false);
+  // useEffect(() => {
+  //   if (cards.filter((card) => card == null).length === 0) {
+  //     setIsEnd(true);
+  //   }
+  // });
   return (
-    <div>
-      <Card />
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      {cards.filter((card) => card !== null).length === 0 ? (
+        <div>
+          <p>congrats biatch</p>
+          <button onClick={() => generate()}>Restart</button>
+        </div>
+      ) : (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+            gap: "10px",
+          }}
+        >
+          {cards.map((card, index) => {
+            return (
+              <Card
+                card={card}
+                flip={() => flip(index)}
+                isFlipped={flipped[index]}
+                key={index}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
--- DC/flipGame/flip/src/components/Card.jsx
+++ DC/flipGame/flip/src/components/Card.jsx
@@ -1,11 +1,62 @@
 import React from "react";
-export const Card = () => {
-  <div>
-    <div>
-      <img src="" alt="front" />
+import CardBack from "../assets/CardBack.png";
+export const Card = ({ card, flip, isFlipped }) => {
+  if (!card) {
+    return (
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+        }}
+      />
+    );
+  }
+  return (
+    <div
+      onClick={() => flip()}
+      style={{
+        width: "200px",
+        height: "200px",
+        position: "relative",
+        transform: `rotateY(${isFlipped ? "180deg" : "0deg"})`,
+        transformStyle: "preserve-3d",
+        transition: "transform 0.6s",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          backfaceVisibility: "hidden",
+        }}
+      >
+        <img
+          src={CardBack}
+          alt="front"
+          style={{
+            width: "100%",
+            borderRadius: "20px",
+          }}
+        />
+      </div>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          backfaceVisibility: "hidden",
+          transform: "rotateY(180deg)",
+        }}
+      >
+        <img
+          src={`${card}.png`}
+          alt="back"
+          style={{
+            width: "100%",
+            borderRadius: "20px",
+          }}
+        />
+      </div>
     </div>
-    <div>
-      <img src="" alt="back" />
-    </div>
-  </div>;
+  );
 };
