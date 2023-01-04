fa8772b7213d1c79d1a7bd85580780c81ddbe768
Uda <uda@Udas-MacBook-Pro.local>
2023-01-04 12:20:05 +0800

    .

--- NME/Boginoo/boginoo/src/pages/homepage.jsx
+++ NME/Boginoo/boginoo/src/pages/homepage.jsx
@@ -8,21 +8,31 @@ import Logo from "../assets/logo.png";
 
 export const HomePage = () => {
   return (
-    <div>
+    <div className="container">
       <div>
-        <Button variant="text">ХЭРХЭН АЖИЛЛАДАГ ВЭ?</Button>
-        <Button variant="contained">НЭВТРЭХ</Button>
-      </div>
-      <Container>
         <div>
-          <div>
-            <img src={LogoLeft} alt="logoleft" />
-            <img src={Logo} alt="logo" />
-            <img src={LogoRight} alt="logoright" />
-          </div>
-          <div>Boginoo</div>
+          <button className="button">ХЭРХЭН АЖИЛЛАДАГ ВЭ?</button>
+          <button className="containedbutton">НЭВТРЭХ</button>
         </div>
-      </Container>
+      </div>
+      <div className="mainPartContainer">
+        <div className="logocontainer">
+          <div className="logoPicContainer">
+            <img src={LogoLeft} alt="logoleft" className="logoleft" />
+            <img src={Logo} alt="logo" className="logo" />
+            <img src={LogoRight} alt="logoright" className="logoright" />
+          </div>
+          <div className="boginoologo">Boginoo</div>
+        </div>
+        <div className="formContainer">
+          <input placeholder="https://www.web-huudas.mn" />
+          <button className="containedbutton">БОГИНОСГОХ</button>
+        </div>
+      </div>
+      <div className="footer">
+        <p>Made with ♥️ by Udval</p>
+        <div>©boginooo 2023</div>
+      </div>
     </div>
   );
 };
