989e619eef96dc76410748a04f47da40b1295567
Uda <uda@Udas-MacBook-Pro.local>
2023-01-10 23:49:38 +0800

    log in

--- NME/Boginoo/frontend/src/pages/Homepage.jsx
+++ NME/Boginoo/frontend/src/pages/Homepage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { LogoBoginoo } from "../components/Logo";
 import axios from "axios";
-import { Link } from "react-router-dom";
 
 export const HomePage = () => {
   const [original, setOriginal] = useState("");
--- NME/Boginoo/frontend/src/pages/Login.jsx
+++ NME/Boginoo/frontend/src/pages/Login.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { LogoBoginoo } from "../components/Logo";
+import { useState } from "react";
+import axios from "axios";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.post("http://localhost:9000/user/login", {
+        email: email,
+        password: password,
+      });
+    } catch (error) {
+      alert(error);
+    }
+  };
   return (
     <div className="signupcontainer">
       <LogoBoginoo />
@@ -26,13 +41,25 @@ export const Login = () => {
           className="signupinput"
           type="text"
           placeholder="name@mail.domain"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
       </div>
       <div>
         <p>Нууц үг</p>
-        <input className="signupinput" type="password" placeholder="••••••••" />
+        <input
+          className="signupinput"
+          type="password"
+          placeholder="••••••••"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
       </div>
-      <button className="containedbutton buttonsigup">НЭВТРЭХ</button>
+      <button className="containedbutton buttonsigup" onClick={handleSubmit}>
+        НЭВТРЭХ
+      </button>
       <Link to={"/signup"}>Шинэ хэрэглэгч бол энд дарна уу?</Link>
     </div>
   );
